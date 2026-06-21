a52beed91a97524d3a4cb6fdd7a0ad53aa07fc83
D3BB13-webdev <debbieallap@gmail.com>
2026-06-21 16:24:33 +0100

    moved lesson one files into js1 folder and finished lesson two in js2 folder

---
+++ js2/js/main.js
@@ -0,0 +1,22 @@
+// Strings
+const myVariable = "Mathematics";
+
+// Length Property
+console.log(myVariable.length);
+
+// String Methods
+console.log(myVariable.charAt(6));
+console.log(myVariable.indexOf("at"));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(2));
+console.log(myVariable.slice(2, 5));
+console.log(myVariable.slice(5, 2));
+console.log(myVariable.slice(2, 3));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("them"));
+console.log(myVariable.includes("div"));
+console.log(myVariable.split("a"));
+console.log(myVariable.split(""));
+console.log("Jhon,Joe,Dave,Debbie".split(","))
+console.log("Every good girl does fine".split(" "))
