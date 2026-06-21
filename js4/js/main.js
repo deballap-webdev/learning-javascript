8c1b5637baa007479b8936dc7a016a85d85b5e6c
D3BB13-webdev <debbieallap@gmail.com>
2026-06-21 20:39:41 +0100

    javascript lesson 4 -Math Methods and Properties

---
+++ js4/js/main.js
@@ -0,0 +1,33 @@
+// Math Methods and Properties
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI));
+console.log(Math.round(3.4));
+console.log(Math.round(3.5));
+console.log(Math.ceil(3.5));
+console.log(Math.ceil(Math.PI));
+console.log(Math.ceil(3.0));
+console.log(Math.floor(3.9));
+console.log(Math.pow(2, 4));
+console.log(Math.pow(5, 2));
+console.log(Math.min(2, 5, 6));
+console.log(Math.max(2, 5, 6));
+console.log(Math.max(2, 5, 6.3, Math.ceil(6.1)));
+console.log(Math.max(2, 5, 6.3, Math.floor(6.9)));
+console.log(Math.min(2, 5, 6.3, Math.floor(2.9)));
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+// From 1 to 10
+console.log((Math.floor(Math.random() * 10) + 1));
+console.log((Math.floor(Math.random() * 10) + 1));
+console.log((Math.floor(Math.random() * 10) + 1));
+console.log((Math.floor(Math.random() * 10) + 1));
+console.log((Math.floor(Math.random() * 10) + 1));
+
