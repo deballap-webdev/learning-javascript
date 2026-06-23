42b1528e56bdba1952f63b5cf4af46e8a99d9963
D3BB13-webdev <debbieallap@gmail.com>
2026-06-23 17:46:12 +0100

    learned more about scope  with examples and how var differs from let and const locally

--- js13/js/main.js
+++ js13/js/main.js
@@ -8,19 +8,39 @@ var x = 2;
 console.log(x); */
 
 // global scope
-var x = 1;
-let y = 2;
-const z = 3;
+//var x = 1;
+//let y = 2;
+//const z = 3;
 
 // local scope (function scope)
-const myFunc = () => {
+//const myFunc = () => {
+//local scope (block scope){let y = 4;console.log(y);console.log(z);}};myFunc();}
+// const z = 5; }}
+
+// global scope
+var x = 1; //function scoped
+let y = 2; // block scoped
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+const myFunc = function () {
+  var x = 10;
   const z = 5;
 
-  //local scope (block scope)
   {
-    let y = 4;
-    console.log(y);
-    console.log(z);
+    var x = 11; //function scoped
+    const z = 6; //block scoped
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
   }
+
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
 };
+
 myFunc();
