bcf733b37c0543791429672e709127198b098767
D3BB13-webdev <debbieallap@gmail.com>
2026-06-26 19:02:10 +0100

    JS lesson 18 -JavaScript Object Notation (JSON)

---
+++ js18/js/main.js
@@ -0,0 +1,26 @@
+//JSON: JavaScript Object Notation
+/* JSON is used to send and recieve data.
+JSON is a text format that is completely language independent.
+Meaning JSON is used to send & recieve files in data in many languages, not just in JavaScript*/
+
+const myObj = {
+  name: "Debbie",
+  hobbies: ["eat", "sleep", "code"],
+  hello: function () {
+    console.log("Hello!");
+  },
+};
+
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
