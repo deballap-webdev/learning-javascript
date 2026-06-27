d7c18236eb43d7571bb68f848abb1320c881de5b
D3BB13-webdev <debbieallap@gmail.com>
2026-06-27 11:58:53 +0100

    JS lesson 19 -JavaScript Errors and Error Handling

---
+++ js19/js/main.js
@@ -0,0 +1,39 @@
+// JavaScript Errors and Error Handling
+"use strict";
+
+// reference error
+/* variable = "Hello";
+console.log(hello); */
+
+// syntax error
+/* Object..create(); */
+
+// type error
+/* const name = "Debbie";
+name = "Deborah"; */
+
+const makeError = () => {
+  let i = 1;
+  while (i <= 5) {
+    try {
+      if (i % 2 !== 0) {
+        throw new Error("Odd number!");
+      }
+      console.log("Even number!");
+    } catch (err) {
+      console.error(err.stack);
+    } finally {
+      console.log("...finally");
+      i++;
+    }
+  }
+};
+
+makeError();
+/* 
+function customError(message) {
+  this.message = message;
+  this.name = "customError";
+  this.stack = `${this.name} : ${this.message}`;
+}
+ */
