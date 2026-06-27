c0db1391e9ff4dbd82bbacb54817b88d56fe92f7
D3BB13-webdev <debbieallap@gmail.com>
2026-06-27 21:38:58 +0100

    JS lesson 20 -DOM: selecting elements and changing element styles

---
+++ js20/js/main.js
@@ -0,0 +1,38 @@
+// DOM - Document Object Model
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+const view2 = document.querySelector("#view2");
+console.log(view2);
+
+view1.style.display = "flex";
+view2.style.display = "none";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+
+for (let i = 0; i < evenDivs.length; i++) {
+  evenDivs[i].style.backgroundColor = "darkblue";
+  /* evenDivs[i].style.width = "200px";
+  evenDivs[i].style.height = "200px"; */
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+navText.textContent = "Hello World!";
+
+const navbar = document.querySelector("nav");
+navbar.innerHTML = "<h1>Hello!</h1> <p>This should align right</p>";
+
+console.log(navbar);
+navbar.style.justifyContent = "space-between";
