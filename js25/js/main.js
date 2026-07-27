42005bbac29ac1287e882a5a3d3fe86f394da120
deballap-webdev <debbieallap@gmail.com>
2026-07-27 10:45:10 +0100

    workflow function example on fetch apis

--- js25/js/main.js
+++ js25/js/main.js
@@ -64,9 +64,35 @@ console.log(users);
  */
 
 // Async / Await
-
+/* const myUsers = {
+  userList: [],
+};
 const myCoolFunction = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const userData = await response.json();
   return userData;
 };
+
+const anotherFunc = async () => {
+  const data = await myCoolFunction();
+  myUsers.userList = data;
+  console.log(myUsers.userList);
+};
+
+anotherFunc();
+console.log(myUsers.userList);
+ */
+
+// More Examples
+const getUserEmails = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonUserData = await response.json();
+  const userEmailArray = jsonUserData.map((user) => user.email);
+  postToWebPage(userEmailArray);
+};
+
+const postToWebPage = (data) => {
+  console.log(data);
+};
+
+getUserEmails();
