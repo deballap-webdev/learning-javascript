082c4cacbfee107d10f676f8662a7f3ba369ac85
deballap-webdev <debbieallap@gmail.com>
2026-07-28 10:46:33 +0100

    abstract fetch and post joke into functions

--- js25/js/main.js
+++ js25/js/main.js
@@ -84,7 +84,8 @@ console.log(myUsers.userList);
  */
 
 // More Examples
-const getUserEmails = async () => {
+//workflow Function
+/* const getUserEmails = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const jsonUserData = await response.json();
   const userEmailArray = jsonUserData.map((user) => user.email);
@@ -95,4 +96,85 @@ const postToWebPage = (data) => {
   console.log(data);
 };
 
-getUserEmails();
+getUserEmails(); */
+
+//2nd parameter of fetch
+//Using "GET" method
+/* const getDakeJoke = async () => {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const jsonJokeData = await response.json();
+  console.log(jsonJokeData);
+};
+
+getDakeJoke(); */
+
+//Using "POST" method
+/* 
+const jokeObject = {
+  id: "JmGtkyIJeqc",
+  joke: "So a duck walks into a pharmacy and says “Give me some chap-stick… and put it on my bill”",
+};
+
+const postObject = async (jokeObj) => {
+  const response = await fetch("https://httpbin.org/post", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jokeObj),
+  });
+
+  const jsonResponse = await response.json();
+  console.log(jsonResponse.headers);
+};
+
+postObject(jokeObject);
+ */
+/* 
+const requestJoke = async (name) => {
+  const response = await fetch(
+    `https://api.chucknorris.io/jokes/random?name=${name}&category=dev`,
+  );
+  const jsonResponse = await response.json();
+  console.log(jsonResponse.value);
+};
+
+requestJoke("Daniel Moris");
+ */
+//Abstract Into Function
+
+const getDataFromForm = () => {
+  const requestObject = { name: "Bruce Lee", category: "dev" };
+  return requestObject;
+};
+
+const buildRequestUrl = (requestData) => {
+  const url = `https://api.chucknorris.io/jokes/random?name=${requestData.name}&category=${requestData.category}`;
+  return url;
+};
+
+const requestJoke = async (url) => {
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+  const joke = jsonResponse.value;
+  postJokeToPage(joke);
+};
+
+const postJokeToPage = (joke) => {
+  console.log(joke);
+};
+
+requestJoke(buildRequestUrl(getDataFromForm()));
+
+//Procedural
+
+const processJokeRequest = () => {
+  const requestData = getDataFromForm();
+  const url = buildRequestUrl(requestData);
+  requestJoke(url);
+};
